b47a3b7ee25bfea972456886376e303f7cf8369e
Ben Horne <benhorne44@gmail.com>
2013-09-17 16:18:40 -0600

    Added a new exercise.

--- new_exercises_test.rb
+++ new_exercises_test.rb
@@ -5,6 +5,7 @@ require 'minitest/pride'
 
 class NewExercisesTest < Minitest::Test
   
+
   def test_pick_numbers_divisible_by_two_and_three
     numbers = (0..30)
     selected = []
@@ -15,5 +16,6 @@ class NewExercisesTest < Minitest::Test
     end
     assert_equal [0, 6, 12, 18, 24, 30], selected 
   end
+
 
 end
