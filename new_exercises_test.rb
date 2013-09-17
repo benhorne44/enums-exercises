465d54d6380c5533101b74683b0be096838b2122
Ben Horne <benhorne44@gmail.com>
2013-09-17 16:10:51 -0600

    Added a new exercise.

---
+++ new_exercises_test.rb
@@ -0,0 +1,18 @@
+gem 'minitest'
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class NewExercisesTest < Minitest::Test
+  def test_pick_numbers_divisible_by_two_and_three
+    numbers = (0..30)
+    selected = []
+    numbers.each do |number|
+      if number % 2 == 0 && number % 3 ==0
+        selected.push(number)
+      end
+    end
+    assert_equal [0, 6, 12, 18, 24, 30], selected 
+  end
+
+end
